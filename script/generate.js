db9958c788b3e24898266550a3c593ac6106a0d0
teppeis <teppeis@gmail.com>
2017-03-10 02:59:28 +0900

    refactor: concat continuous lines

--- script/generate.js
+++ script/generate.js
@@ -9,25 +9,49 @@ fetch(URL)
   .then(res => readline.createInterface({input: res.body}))
   .then(lines => {
     let n = 0;
+    let prevStart;
+    let prevEnd;
+    let prevType;
     lines.on('line', line => {
       if (n === 0) {
-        console.log(`'use strict';`);
         console.log(line.replace(/^# (EastAsianWidth-.*)$/, '// $1'));
+        console.log(`'use strict';`);
         console.log('module.exports = [');
       } else {
         const match = /^([0-9A-Z]+)(?:\.\.([0-9A-Z]+))?;(\w+)/.exec(line);
         if (match) {
           const [, start, end, type] = match;
-          if (end) {
-            console.log(`['${type}', 0x${start}, 0x${end}],`);
+          if (type === prevType &&
+              ((prevEnd && Number(`0x${prevEnd}`) + 1 === Number(`0x${start}`)) ||
+              (!prevEnd && Number(`0x${prevStart}`) + 1 === Number(`0x${start}`)))) {
+            // concat this line with previous line
+            if (end) {
+              prevEnd = end;
+            } else {
+              prevEnd = start;
+            }
           } else {
-            console.log(`['${type}', 0x${start}],`);
+            if (prevStart) {
+              // output previous line
+              outputLine(prevStart, prevEnd, prevType);
+            }
+            [prevStart, prevEnd, prevType] = [start, end, type];
           }
         }
       }
       n++;
     });
     lines.on('close', () => {
+      // output last line
+      outputLine(prevStart, prevEnd, prevType);
       console.log('];');
     });
   });
+
+function outputLine(start, end, type) {
+  if (end) {
+    console.log(`['${type}', 0x${start}, 0x${end}],`);
+  } else {
+    console.log(`['${type}', 0x${start}],`);
+  }
+}
