efb0452f60d605c328140740e6cd69ef63fcc65c
teppeis <teppeis@gmail.com>
2022-07-13 01:23:01 +0900

    refactor: mv generate.js generate.mjs

---
+++ script/generate.mjs
@@ -0,0 +1,64 @@
+const URL = "https://www.unicode.org/Public/14.0.0/ucd/EastAsianWidth.txt";
+
+const res = await fetch(URL);
+const text = await res.text();
+const lines = text.split("\n");
+let n = 0;
+let prevStart;
+let prevEnd;
+let prevType;
+const types = new Map();
+
+for (const line of lines) {
+  if (n === 0) {
+    console.log(line.replace(/^# (EastAsianWidth-.*)$/, "// $1"));
+    console.log("exports.data = [");
+  } else {
+    const match = /^([0-9A-Z]+)(?:\.\.([0-9A-Z]+))?;(\w+)/.exec(line);
+    if (match) {
+      const [, start, end, type] = match;
+      if (
+        type === prevType &&
+        ((prevEnd && Number(`0x${prevEnd}`) + 1 === Number(`0x${start}`)) ||
+          (!prevEnd && Number(`0x${prevStart}`) + 1 === Number(`0x${start}`)))
+      ) {
+        // concat this line with previous line
+        if (end) {
+          prevEnd = end;
+        } else {
+          prevEnd = start;
+        }
+      } else {
+        if (prevStart) {
+          // output previous line
+          outputLine(prevStart, prevEnd, prevType);
+        }
+        [prevStart, prevEnd, prevType] = [start, end, type];
+      }
+    }
+  }
+  n++;
+}
+
+// output last line
+outputLine(prevStart, prevEnd, prevType);
+console.log("];");
+const typesStr = JSON.stringify(Array.from(types.keys()));
+console.log(`exports.types = ${typesStr};`);
+
+function outputLine(start, end, type) {
+  const typeId = getTypeId(type);
+  if (end) {
+    const diff = Number(`0x${end}`) - Number(`0x${start}`);
+    console.log(`[${typeId}, 0x${start}, ${diff}],`);
+  } else {
+    console.log(`[${typeId}, 0x${start}],`);
+  }
+}
+
+function getTypeId(type) {
+  if (!types.has(type)) {
+    types.set(type, types.size);
+  }
+  return types.get(type);
+}
